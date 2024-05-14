6408e19229706189cdac154ceda0eca5f430db67
KonstantinTolmache <koctya150@mail.ru>
2024-05-14 22:10:46 +0500

    lr6.java

--- src/lr6/Example9/Main.java
+++ src/lr6/Example9/Main.java
@@ -4,15 +4,20 @@ import java.util.Arrays;
 
 public class Main {
 
-    public static void getMaxAndMinValue( char[] chars ){
-        Arrays.sort( chars );
-        System.out.println( chars );
-        System.out.println( Arrays.toString( chars ));
+    public static void arrayRevers( char[] chars ){
+
+        for ( int i = 0; i < chars.length / 2; i++ ) {
+            char tmp = chars[ i ];
+            chars[ i ] = chars[ chars.length - i - 1 ];
+            chars[ chars.length - i - 1 ] =  tmp;
+        }
+
     }
 
     public static void main( String[] args ) {
-        char[] chars = { 'e', 'd', 'c', 'b', 'a' };
-        getMaxAndMinValue( chars );
+        char[] chars = { 'e', 'i', 'c', 'g', 'a', 'f' };
+        arrayRevers( chars );
+        System.out.println( Arrays.toString( chars ));
     }
 
 }
